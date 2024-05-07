ffdcd284f5cba10d92204022e9edcf519009be05
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-05-07 17:08:55 +0200

    export Factory function per il loader delle traduzioni HTTP ed import in app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; // Moduli per la gestione delle richieste HTTP
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'; // Loader per le traduzioni HTTP
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+// Factory function per il loader delle traduzioni HTTP
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
+// Definizione del modulo principale dell'applicazione
 @NgModule({
   declarations: [
     AppComponent
@@ -13,9 +19,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
+    HttpClientModule, // Importa il modulo per la gestione delle richieste HTTP
   ],
-  providers: [],
+  providers: [], // Fornitori di servizi
   bootstrap: [AppComponent]
 })
 export class AppModule { }
